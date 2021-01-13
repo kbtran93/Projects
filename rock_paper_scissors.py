d9ec547896d0020944900d5f6eee69a93e9b0cca
Binh Tran <binhtran.k50@gmail.com>
2021-01-13 19:59:43 +0100

    Create rock_paper_scissors.py

---
+++ rock_paper_scissors.py
@@ -0,0 +1,35 @@
+from random import randint
+
+#Play options
+t = ['Rock', 'Paper', 'Scissors']
+
+#Assign a random play to the computer
+computer = t[randint(0,2)]
+
+#Set player to False
+player = False
+
+while player == False:
+#Set player to True
+    player = input('Rock, Paper, Scissors?')
+    if player == computer:
+        print('Tie!')
+    elif player =='Rock':
+        if computer =='Paper':
+            print('You lose!', computer, 'covers', player)
+        else:
+            print('You win!', player, 'smashes', computer)
+    elif player == 'Paper':
+        if computer == 'Scissors':
+            print('You lose!', computer, 'cuts', player)
+        else:
+            print('You win!', player, 'covers', computer)
+    elif player == 'Scissors':
+        if computer == 'Rock':
+            print('You lose!', computer, 'smashes', player)
+        else:
+            print('You win!', player, 'cuts', computer)
+    else:
+        print('Invallid input. PLease check your spelling!')
+    player = False
+    computer = t[randint(0,2)]
